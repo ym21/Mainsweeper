cd97f2908a7f1f112624f822f676996769e419fb
ym21 <44824384+ym21@users.noreply.github.com>
2021-03-21 00:47:26 +0900

    edit

--- src/js/main.js
+++ src/js/main.js
@@ -1,53 +1,53 @@
 import { Mainsweeper } from "./Mainsweeper.js";
 
 class Game {
-	constructor() {
-		this.mainDisp = document.getElementById("main");
+    constructor() {
+        this.mainDisp = document.getElementById("main");
 
-		this.game = new Mainsweeper();
+        this.game = new Mainsweeper();
 
-		this.game.addEventListener("update", () => {
-			this.update();
-		});
-		this.game.addEventListener("start", () => {
-			this.start();
-		});
-		this.game.addEventListener("end", () => {
-			this.end();
-		});
+        this.game.addEventListener("update", () => {
+            this.update();
+        });
+        this.game.addEventListener("start", () => {
+            this.start();
+        });
+        this.game.addEventListener("end", () => {
+            this.end();
+        });
 
-		this.cells = null;
-	}
-	init() {
-		this.game.init(10, 10, 10);
-		this.mainDisp.innerHTML = "";
-		this.cells = [];
-		for (let y = 0; y < this.game.rows; y++) {
-			this.cells[y] = [];
-			for (let x = 0; x < this.game.columns; x++) {
-				this.cells[y][x] = document.createElement("div");
-				this.cells[y][x].className = "cell";
-				this.mainDisp.appendChild(this.cells[y][x]);
-			}
-		}
-	}
-	start() {}
+        this.cells = null;
+    }
+    init() {
+        this.game.init(10, 10, 10);
+        this.mainDisp.innerHTML = "";
+        this.cells = [];
+        for (let y = 0; y < this.game.rows; y++) {
+            this.cells[y] = [];
+            for (let x = 0; x < this.game.columns; x++) {
+                this.cells[y][x] = document.createElement("div");
+                this.cells[y][x].className = "cell";
+                this.mainDisp.appendChild(this.cells[y][x]);
+            }
+        }
+    }
+    start() {}
 
-	update() {
-		for (const [r, rc] of this.cells.entries()) {
-			for (const [c, cell] of rc.entries()) {
-				const state = this.game.cells[(r, c)];
-				cell.className = "cell";
-			}
-		}
-	}
+    update() {
+        for (const [r, rc] of this.cells.entries()) {
+            for (const [c, cell] of rc.entries()) {
+                const state = this.game.cells[(r, c)];
+                cell.className = "cell";
+            }
+        }
+    }
 
-	end() {}
+    end() {}
 }
 
 //読み込み完了後実行
 document.addEventListener("DOMContentLoaded", () => {
-	const g = new Game();
-	g.init();
-	g.update();
+    const g = new Game();
+    g.init();
+    g.update();
 });
