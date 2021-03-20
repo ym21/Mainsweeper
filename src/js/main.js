6bd9f522a28fad064d683edb845447e0232ef20e
ym21 <44824384+ym21@users.noreply.github.com>
2021-03-20 18:46:55 +0900

    edit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-console.log("test");
+import { Mainsweeper } from "./Mainsweeper.js";
+
+//読み込み完了後実行
+document.addEventListener("DOMContentLoaded", () => {
+	const m = new Mainsweeper();
+	m.addEventListener("update", () => {
+		console.log("test");
+	});
+});
