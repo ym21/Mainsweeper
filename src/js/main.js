d55ecea4ac5122cc04663dd10f569dbb78394b6f
ym21 <44824384+ym21@users.noreply.github.com>
2021-03-21 00:22:27 +0900

    edit

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,53 @@
 import { Mainsweeper } from "./Mainsweeper.js";
 
 class Game {
-    constructor() {
-        this.main = document.getElementById("main");
-        this.game = new Mainsweeper();
-        this.game.addEventListener("update", this.update);
-        this.game.addEventListener("gameClear", this.gameClear);
-        this.game.addEventListener("gameOver", this.gameOver);
-    }
-    init() {}
+	constructor() {
+		this.mainDisp = document.getElementById("main");
 
-    update() {}
+		this.game = new Mainsweeper();
 
-    gameClear() {}
+		this.game.addEventListener("update", () => {
+			this.update();
+		});
+		this.game.addEventListener("start", () => {
+			this.start();
+		});
+		this.game.addEventListener("end", () => {
+			this.end();
+		});
 
-    gameOver() {}
+		this.cells = null;
+	}
+	init() {
+		this.game.init(10, 10, 10);
+		this.mainDisp.innerHTML = "";
+		this.cells = [];
+		for (let y = 0; y < this.game.rows; y++) {
+			this.cells[y] = [];
+			for (let x = 0; x < this.game.columns; x++) {
+				this.cells[y][x] = document.createElement("div");
+				this.cells[y][x].className = "cell";
+				this.mainDisp.appendChild(this.cells[y][x]);
+			}
+		}
+	}
+	start() {}
+
+	update() {
+		for (const [r, rc] of this.cells.entries()) {
+			for (const [c, cell] of rc.entries()) {
+				const state = this.game.cells[(r, c)];
+				cell.className = "cell";
+			}
+		}
+	}
+
+	end() {}
 }
 
 //読み込み完了後実行
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+	const g = new Game();
+	g.init();
+	g.update();
+});
