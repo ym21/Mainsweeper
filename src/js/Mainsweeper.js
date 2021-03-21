982e23b72b9ec920cd66d535f1803bfc9d3a7609
ym21 <44824384+ym21@users.noreply.github.com>
2021-03-21 21:43:17 +0900

    v0.0.1

--- src/js/Mainsweeper.js
+++ src/js/Mainsweeper.js
@@ -18,12 +18,22 @@ export class Mainsweeper extends EventDispatcher {
         this.columns = 0; //ゲームのサイズ（列）
         this.bombs = 0; //爆弾の数
         this.cells = null; //セルの状態
+        this.isUpdate = false; //更新の有無
+        this.flagCnt = 0; //フラグの数
+        this.isEnd = false; //ゲームが終わっているか
+        this.isClear = false; //クリアの有無
+        this.isStart = false; //ゲームの開始
     }
 
+    //初期化
     init(rows, columns, bombs) {
         this.rows = rows;
         this.columns = columns;
         this.bombs = bombs;
+        this.flagCnt = 0;
+        this.isEnd = false;
+        this.isClear = false;
+        this.isStart = false;
         this.cells = [];
         for (let y = 0; y < this.rows; y++) {
             this.cells[y] = [];
@@ -33,12 +43,124 @@ export class Mainsweeper extends EventDispatcher {
         }
     }
 
+    //セルを開く
     openCell(row, column) {
-        const cell = this.cells[row][column];
-        if (cell && !cell.isFlag && !cell.isOpen) {
-            cell.isOpen = true;
+        if (!this.isStart) {
+            this.setBombs(row, column);
+            this.start();
         }
+        if (!this.isEnd && !this.cells[row][column].isFlag) {
+            this._openCell(row, column);
+            if (this.isUpdate) {
+                this.update();
+            }
+        }
+    }
 
+    _openCell(row, column) {
+        if (this.cells[row] && this.cells[row][column]) {
+            const cell = this.cells[row][column];
+            if (cell.isBomb) {
+                this.gameEnd();
+                return;
+            } else if (!cell.isFlag && !cell.isOpen) {
+                cell.isOpen = true;
+                this.isUpdate = true;
+                if (cell.arndBombCnt === 0) {
+                    this._openCell(row - 1, column - 1);
+                    this._openCell(row - 1, column);
+                    this._openCell(row - 1, column + 1);
+                    this._openCell(row, column - 1);
+                    this._openCell(row, column + 1);
+                    this._openCell(row + 1, column - 1);
+                    this._openCell(row + 1, column);
+                    this._openCell(row + 1, column + 1);
+                }
+            }
+        }
+    }
+
+    //爆弾を設置 r,cにはおかない
+    setBombs(row, column) {
+        const randMax = this.rows * this.columns - 1;
+        const numsList = new Int8Array(randMax + 1).map((v, i) => i); //ここからランダムに取り出す
+
+        numsList[row * this.columns + column] = numsList[randMax]; //(r,c)をリストから削除
+
+        for (let i = randMax; i > randMax - this.bombs; i--) {
+            const rand = Math.floor(Math.random() * i);
+
+            const y = Math.floor(numsList[rand] / this.columns);
+            const x = numsList[rand] % this.columns;
+
+            this.cells[y][x].isBomb = true; //爆弾設置
+            this._setAroundBombs(y, x);
+
+            numsList[rand] = numsList[i - 1]; //一度出た数字はリストから削除
+        }
+    }
+
+    //周囲の爆弾の数を入れる
+    _setAroundBombs(row, column) {
+        for (let y = row - 1; y <= row + 1; y++) {
+            for (let x = column - 1; x <= column + 1; x++) {
+                if (y >= 0 && y < this.rows && x >= 0 && x < this.columns) {
+                    this.cells[y][x].arndBombCnt++;
+                }
+            }
+        }
+    }
+
+    //フラグ設定
+    toggleFlag(row, column) {
+        const cell = this.cells[row][column];
+        if (this.isStart && !this.isEnd && !cell.isOpen) {
+            if (cell.isFlag) {
+                this.flagCnt--;
+                cell.isFlag = false;
+            } else {
+                this.flagCnt++;
+                cell.isFlag = true;
+            }
+            this.update();
+        }
+    }
+
+    //クリアチェック
+    clearCheck() {
+        for (const rc of this.cells) {
+            for (const c of rc) {
+                if (!c.isBomb && !c.isOpen) {
+                    return;
+                }
+            }
+        }
+        this.gameClear();
+    }
+
+    //ゲーム開始
+    start() {
+        this.isStart = true;
+        this.dispatchEvent(new Event("start"));
+    }
+
+    //更新
+    update() {
+        this.clearCheck();
         this.dispatchEvent(new Event("update"));
+        this.isUpdate = false;
+    }
+
+    //ゲーム終了
+    gameEnd() {
+        this.isEnd = true;
+        this.dispatchEvent(new Event("end"));
+    }
+
+    //ゲームクリア
+    gameClear() {
+        this.isEnd = true;
+        this.isClear = true;
+        this.dispatchEvent(new Event("end"));
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -2,24 +2,38 @@ import { Mainsweeper } from "./Mainsweeper.js";
 
 class Game {
     constructor() {
-        this.mainDisp = document.getElementById("main");
+        this.mainDisp = document.getElementById("game");
+        this.resetButton = document.getElementById("reset-button");
+        this.timeLabel = document.getElementById("time");
+        this.bombLabel = document.getElementById("bomb");
 
         this.game = new Mainsweeper();
 
-        this.game.addEventListener("update", () => {
-            this.update();
-        });
         this.game.addEventListener("start", () => {
             this.start();
+        });
+        this.game.addEventListener("update", () => {
+            this.update();
         });
         this.game.addEventListener("end", () => {
             this.end();
         });
+        this.resetButton.addEventListener("click", () => {
+            this.end();
+            this.init();
+        });
 
         this.cells = null;
+        this.timer = null;
+        this.time = 0;
     }
     init() {
-        this.game.init(10, 10, 10);
+        this.game.init(10, 10, 20);
+        this.timer = null;
+        this.time = 0;
+        this.timeLabel.innerHTML = "⏱  0";
+        this.bombLabel.innerHTML =
+            "💣" + ("   " + (this.game.bombs - this.game.flagCnt)).slice(-3);
         this.mainDisp.innerHTML = "";
         this.cells = [];
         for (let y = 0; y < this.game.rows; y++) {
@@ -28,26 +42,67 @@ class Game {
                 this.cells[y][x] = document.createElement("div");
                 this.cells[y][x].className = "cell";
                 this.mainDisp.appendChild(this.cells[y][x]);
+                this.cells[y][x].onclick = () => {
+                    this.game.openCell(y, x);
+                };
+                this.cells[y][x].oncontextmenu = () => {
+                    this.game.toggleFlag(y, x);
+                };
             }
         }
     }
-    start() {}
+    start() {
+        this.timer = setInterval(() => {
+            this.time++;
+            this.timeLabel.innerHTML = "⏱" + ("   " + this.time).slice(-3);
+        }, 1000);
+    }
 
     update() {
         for (const [r, rc] of this.cells.entries()) {
             for (const [c, cell] of rc.entries()) {
-                const state = this.game.cells[(r, c)];
-                cell.className = "cell";
+                const state = this.game.cells[r][c];
+                if (!state.isFlag) {
+                    cell.innerHTML = "";
+                } else {
+                    cell.className = "cell flag";
+                    cell.innerHTML = "🚩";
+                }
+                if (state.isOpen) {
+                    cell.className = "cell open";
+                    cell.innerHTML =
+                        state.arndBombCnt === 0 ? "" : state.arndBombCnt;
+                }
+            }
+        }
+        this.bombLabel.innerHTML =
+            "💣" + ("   " + (this.game.bombs - this.game.flagCnt)).slice(-3);
+    }
+
+    end() {
+        clearInterval(this.timer);
+        if (this.game.isClear) {
+            console.log("Game Clear");
+        } else {
+            console.log("Game Over");
+            for (const [r, rc] of this.cells.entries()) {
+                for (const [c, cell] of rc.entries()) {
+                    const state = this.game.cells[r][c];
+                    if (state.isBomb) {
+                        cell.className = "cell bomb";
+                        cell.innerHTML = "💣";
+                    } else if (state.isFlag) {
+                        cell.className = "cell miss";
+                        cell.innerHTML = "✖";
+                    }
+                }
             }
         }
     }
-
-    end() {}
 }
 
 //読み込み完了後実行
 document.addEventListener("DOMContentLoaded", () => {
     const g = new Game();
     g.init();
-    g.update();
 });
